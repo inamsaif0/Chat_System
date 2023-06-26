97c649f0912a4a1ba98995895eae7df6c3744b7c
MaaZ Asim <maazasim@MaaZs-MacBook-Pro.local>
2023-06-26 17:12:16 +0500

    added client

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+import Chat from './components/Chat'
+function App() {
+  return (
+    <div className="App">
+      <Chat />
+      </div>
+  );
+}
+
+export default App;
